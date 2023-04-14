41c68b41ac08bdf9861902193d9251bf415d59f2
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-13 22:43:01 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,26 +1,12 @@
-import Laboratorio5;
+import Laboratorio6;
 import pytest;
-    
-    
-def test_sumaImparesPares_1():
-    assert Laboratorio5.sumaImparesPares([0,2,3,4], [4, 8, 6, 0]) == [13, 14]
-    
-def test_sumaImparesPares_2():
-    assert Laboratorio5.sumaImparesPares([10, 0], [100, 2]) == [110, 2]
-    
-def test_sumaImparesPares_3():
-    assert isinstance(Laboratorio5.sumaImparesPares([1,2], "dos"), str) == isinstance("Error: segundo argumento debe ser entero", str)
-    
+from unittest import mock
+# from unittest.mock import patch
 
-def test_eliminarElemento_1():
-    assert Laboratorio5.eliminarElemento( [12, 78, 12, 0, 5.2, "abc", 0, 12, 5.2, 12], 12 ) == [78, 0, 5.2, "abc", 0, 5.2]
-    
-def test_eliminarElemento_2():
-    assert Laboratorio5.eliminarElemento( [12, 5.2, 12], 5 ) == [12, 5.2, 12]
-    
-    
-def test_eliminarRepetidos_1():
-    assert Laboratorio5.eliminarRepetidos( [12, 78, 12, 0, 5.2, "abc", 0, 12, 5.2, 12] , [12, 5.2, 0]) == [78, "abc"]
-    
-def test_eliminarRepetidos_2():
-    assert Laboratorio5.eliminarRepetidos( [12, 5.2, 12], [12] ) == [5.2]
+modulo = Laboratorio6
+
+def test_convertir_1():  
+    opcion = '2'
+    numero = '7'
+    with mock.patch('builtins.input', side_effect = [opcion, valor]):
+        assert modulo.convertir() == 111
