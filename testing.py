28f206ef655691ed29bdc990f0b98fa8ed31aa9e
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-13 22:52:35 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -10,3 +10,27 @@ def test_convertir_1():
     numero = '7'
     with mock.patch('builtins.input', side_effect = [opcion, numero]):
         assert modulo.convertirBase() == 111
+        
+def test_convertir_2():  
+    opcion = '3'
+    numero = '7'
+    with mock.patch('builtins.input', side_effect = [opcion, numero]):
+        assert modulo.convertirBase() == 21
+
+def test_convertir_3():  
+    opcion = '5'
+    numero = '7000'
+    with mock.patch('builtins.input', side_effect = [opcion, numero]):
+        assert modulo.convertirBase() == '1B58'
+        
+def test_convertir_4():  
+    opcion = '6'
+    numero = '1B58'
+    with mock.patch('builtins.input', side_effect = [opcion, numero]):
+        assert modulo.convertirBase() == 7000
+        
+def test_convertir_5():  
+    opcion = '8'
+    numero = '21'
+    with mock.patch('builtins.input', side_effect = [opcion, numero]):
+        assert modulo.convertirBase() == 7
