daf8b6cb01f2a4e07b5be36e053f9977b1fcdb47
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-13 22:49:16 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,5 +8,5 @@ modulo = Laboratorio6
 def test_convertir_1():  
     opcion = '2'
     numero = '7'
-    with mock.patch('builtins.input', side_effect = [opcion, valor]):
+    with mock.patch('builtins.input', side_effect = [opcion, numero]):
         assert modulo.convertirBase() == 111
